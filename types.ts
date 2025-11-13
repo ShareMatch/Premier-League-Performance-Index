a1d48005b8d47a9379c85c304600f15b0bebf7fb
dev-sharematch <dev@sharematch.me>
2025-11-13 20:02:35 +0000

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,13 @@
+export interface Team {
+  id: number;
+  name: string;
+  bid: number;
+  offer: number;
+  lastChange: 'up' | 'down' | 'none';
+}
+
+export interface Order {
+  team: Team;
+  type: 'buy' | 'sell';
+  price: number;
+}
